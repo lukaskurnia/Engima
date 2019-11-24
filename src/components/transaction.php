b38deb5c11840cf1836b1b9ef50f317a1037cab8
Aldo Azali <13516125@std.stei.itb.ac.id>
2019-11-24 18:36:40 +0700

    connection to ws-bank popup and transaction

--- src/components/transaction.php
+++ src/components/transaction.php
@@ -47,6 +47,8 @@
                     WHERE ord.user_id=? ORDER BY datetime DESC";
         $transactions = $db->execute($sql_query, array("i"), array($user_id));
 
+
+        // GET data from ws-transaction
         $ch = curl_init();
         $url = "http://107.21.9.12:4000/transactions/" . $user_id; //WS-Transaction URL
         curl_setopt($ch, CURLOPT_URL, $url);
@@ -55,12 +57,14 @@
         $output=curl_exec($ch);
         curl_close($ch);
         
-        $rawdata = json_decode($output);
+        $rawdata = json_decode($output); // Data result container
+
+        //Check container data count
         if (count($rawdata->values) == 0) {
             echo "<h3 class='grey-text'>You haven't made any purchase yet.</h3>";
         } else {
-            echo "object preparation begin<br>";
-
+            
+            // Create array to combine data from ws-transaction and db query
             foreach ($rawdata->values as $data) {
                 $data->order_id = -1;
                 $data->review = null;
@@ -77,18 +81,20 @@
                 }
             }
 
+            //Get current time to see wheter the txn has expired or not
             date_default_timezone_set('Asia/Jakarta');
             $curr_date = date('Y-m-d h:i:s', time());
-            // $curr = strtotime(date('m/d/Y h:i:s a', time()));
-            $curr = strtotime($curr_date);
+            $curr = strtotime($curr_date); // Convert datetime format to unix fromat in seconds
 
-            //Prepare connection
+            //Prepare connection to update ws-transaction database
             $url = "http://107.21.9.12:4000/transactions";//WS-Transaction URL
             $ch = curl_init($url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
 
             $index = count($rawdata->values) - 1;
+
+            //Loop for each txn data contained
             for ($idx=$index; $idx>=0; $idx--) {
                 $api_key = '2dc9c50e0d06264a13a9e6953b693bba';
                 $urlGetData = "https://api.themoviedb.org/3/movie/" . $rawdata->values[$idx]->movie_id .
@@ -98,25 +104,72 @@
                 $datetime = strtotime($rawdata->values[$idx]->mov_schedule); //movie schedule
                 $formatted = date('F j, Y - h:i A', $datetime);
 
-                //WS-Transaction
                 $no_transaksi = $rawdata->values[$idx]->txn_id;
                 $status = $rawdata->values[$idx]->txn_status;
-
-                if ($status != "COMPLETED") {
-                    $created_time = strtotime($rawdata->values[$idx]->created_on);
-                    $temp = date('m/d/Y h:i:s a', $created_time);
+                
+                if ($status == "PENDING") {
+                    $created_time = strtotime($rawdata->values[$idx]->created_on);//txn[idx]'s created time
+                    $temp = date('m/d/Y h:i:s a', $created_time); //debuging purpose
                     $diff = abs($curr-$created_time);
+
                     
-                    if ($diff<=120) {
-                        //Koneksi WS-BANK
+                    //Connection to WS-BANK
+                    $expired_time = strtotime($curr_date) + 120;
+                    $formatted_expired_time = date('Y-m-d h:i:s a', $expired_time);
+
+                    $field1 = $rawdata->values[$idx]->virtual_acc; // destination acc number
+                    $field2 = 40000; //fixed amount
+                    $field3 = $created_time;
+                    $field4 = (string)$formatted_expired_time; // txn expired on (start time + 120sec)
+
+                    $checkTransaction = '<?xml version="1.0" encoding="UTF-8"?> <Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
+                        <Body>
+                            <CheckTransaction xmlns="http://services/">
+                                <destRek xmlns="">'.$field1.'</destRek>
+                                <amount xmlns="">'.$field2.'</amount>
+                                <startTime xmlns="">'.$field3.'</startTime>
+                                <endTime xmlns="">'.$field4.'</endTime>
+                            </CheckTransaction>
+                        </Body>
+                    </Envelope>';
+                    $function = $checkTransaction;
+
+                    $feature_route = "CheckTransaction";
+                    $url = "http://localhost:8080/ws-bank-1.0-SNAPSHOT/services/" . $feature_route; //NEED CHANGE
+
+                    $crl = curl_init();
+                    curl_setopt($crl, CURLOPT_URL, $url);
+                    curl_setopt($crl, CURLOPT_RETURNTRANSFER, true);
+                    curl_setopt($crl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+                    curl_setopt($crl, CURLOPT_POST, true);
+                    curl_setopt($crl, CURLOPT_POSTFIELDS, $function);
+                    curl_setopt($crl, CURLOPT_HTTPHEADER, array("Content-Type: text/xml; charset=utf-8", "Content-Length: " . strlen($function)));
+                    $output = curl_exec($crl);
+                    curl_close($crl);
+
+                    $out = <<<EOF
+                    $output
+                    EOF;
+
+                    $parser = simplexml_load_string($out);  
+                    $parserEnv = $parser->children('S',true);
+                    $returns = $parserEnv->Body->children('ns2',true)
+                        ->CheckTransactionResponse->children();
+
+                    $return = $returns->return;
+                    echo "status:  ";
+                    echo $return;
+                    if($return == 200){
                         $status = "COMPLETED";
-                    } else {
+                    } elseif($return == 400){
                         $status = "CANCELLED";
                     }
-                    
-                    $data = array("txn_id"=>$no_transaksi,"txn_status" => $status);
-                    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-                    $response = curl_exec($ch);
+
+                    if($status !="PENDING"){
+                        $data = array("txn_id"=>$no_transaksi,"txn_status" => $status);
+                        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+                        $response = curl_exec($ch);
+                    }  
                 }
 
                 echo "<div class='tp-content__row'>
